2b42e6ca330d117f691eb7621a51fb1fda0904f4
mac <mac@maverick.local>
2020-06-08 00:55:41 +0530

    Icons added

---
+++ src/Components/Icons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+// Check ✅ Icon
+export const check = () => (
+  <Icon name="checkcircle" size={25} color="#1b7f22" />
+);
+// Cross ❌ Icon
+export const cross = () => (
+  <Icon name="closecircle" size={25} color="#b11e1e" />
+);
+// Plus ➕ Icon
+export const plus = () => <Icon name="plus" size={25} color="#6f6f6f" />;
+// Filter ▽ Icon
+export const filterIcon = () => (
+  <Icon name="filter" size={25} color="#6f6f6f" />
+);
