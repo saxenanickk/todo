9a19a0b4abfea89da679b8a9159dcb41198146b9
mac <mac@maverick.local>
2020-06-08 00:56:41 +0530

    getTodos, saveTodos functions added

--- src/customModules/LocalStorage.js
+++ src/customModules/LocalStorage.js
@@ -2,4 +2,17 @@ import {NativeModules} from 'react-native';
 
 const {LocalStorage} = NativeModules;
 
+export const TODOS = '@todos';
+
+export const getTodos = callback =>
+  LocalStorage.getItem(TODOS, data => {
+    if (data) {
+      const fetchedData = JSON.parse(data);
+      callback(fetchedData);
+    }
+  });
+
+export const saveTodos = todos =>
+  LocalStorage.setItem(TODOS, JSON.stringify(todos));
+
 export default LocalStorage;
